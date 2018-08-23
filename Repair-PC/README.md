eeabf06d174acf13082dc22c925970981b6d314d
Daniel Molina <dan-molina@outlook.com>
2018-08-23 16:37:57 -0500

    Templated out

--- Repair-PC/README.md
+++ Repair-PC/README.md
@@ -2,6 +2,10 @@ List of desired features
 
 The idea is an automated script to handle most repair issues automatically
 
+Parameters
+    Acccept DNS Name or IP address of machine to run on remotely
+    Can supply credentials, or will default to user running command
+
 Functions
     Warning to say that all applications will close, then close them.
     Flush Dns
---
+++ Repair-PC/Repair-PC.psm1
@@ -0,0 +1,60 @@
+<#
+
+.SYNOPSIS
+    A one sentence explanation of the script.
+
+.DESCRIPTION
+    Long description
+
+.EXAMPLE
+    Example
+
+.NOTES
+    General notes
+#>
+
+
+#<<<<<<<<<<<<<<<<<<<<      Functions and Parameters     >>>>>>>>>>>>>>>>>>>#
+
+<#
+param (
+    [string]$server = "http://defaultserver", #Parameter of -server with a default value
+    [Parameter(Mandatory = $true)][string]$default, #Parameter that the script will not run without getting. User will be prompted if not given
+    [switch]$force = $false #switch that can be checked for later
+)
+#>
+
+function Test-Administrator {
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+<#function name {
+    param ([type]$parameter1, [type]$parameter2) #Function Parameters
+    #Function Body
+}#>
+
+#<<<<<<<<<<<<<<<<<<<<      Admin Check      >>>>>>>>>>>>>>>>>>>#
+
+if (-NOT (Test-Administrator)) {
+    Write-Host 'Error : Permission Denied : Elevation is required' -ForegroundColor Red
+    Write-Host 'Please run this Script as Administrator' -ForegroundColor Red
+    exit
+}
+
+#<<<<<<<<<<<<<<<<<<<<      Variables and Interaction      >>>>>>>>>>>>>>>>>>>#
+
+$UserCredential = Get-Credential
+$InputFromUser = Read-Host -Prompt 'Ask for something'
+
+#<<<<<<<<<<<<<<<<<<<<      Script Body      >>>>>>>>>>>>>>>>>>>#
+Set-ExecutionPolicy RemoteSigned
+Write-Output $UserCredential
+Write-Output $InputFromUser
+Write-Output $server
+Write-Output $default
+
+# switch example using $force
+if ($force) {
+    Write-Host 'I did the thing'
+}
