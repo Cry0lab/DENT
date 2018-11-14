2b1d9b15a9a85182d80239454102a98396f0166c
Cry0lab <dan-molina@outlook.com>
2018-11-14 15:36:09 -0600

    Stuff

---
+++ Name Reversal/First-Last.ps1
@@ -0,0 +1,29 @@
+#Get all AD Users
+$Users = Get-ADUser -SearchBase "OU=Administrators,OU=Users,OU=Arrow,DC=arrow,DC=org" -Filter {(GivenName -Like "*") -And (Surname -Like "*")} -Properties DisplayName | Select-Object DisplayName, givenName, sn, name, DistinguishedName
+
+#Cycle through each AD User
+ForEach ($User In $Users) {
+
+    #Gather Information about User
+    $DN = $User.DistinguishedName
+    $First = $User.givenName
+    $Last = $User.sn
+    $CN = $User.name
+    $Display = $User.DisplayName
+    $NewName = "$First $Last"
+
+    #Debug Print Outs
+    Write-Output "Current Display Name is $Display"
+    Write-Output "Current CN is $CN"
+
+    #If the Current Display Name is not the New Name, change it to the New Name
+    If ($Display -ne $NewName) {Set-ADUser -Identity $DN -DisplayName $NewName}
+    #If the Current CN is not the New Name, change it to the New Name
+    If ($CN -ne $NewName) {Rename-ADObject -Identity $DN -NewName $NewName}
+
+    #Debug Print Outs
+    $CN = $User.name
+    $Display = $User.DisplayName
+    Write-Output "New Display Name is $Display"
+    Write-Output "New CN is $CN"
+}
