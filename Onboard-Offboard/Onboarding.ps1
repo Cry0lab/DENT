c3ca95dae921530addabd232c115915bf6269e3f
Daniel Molina <dan-molina@outlook.com>
2018-11-16 16:36:52 -0600

    asdf

--- Onboard-Offboard/Onboarding.ps1
+++ Onboard-Offboard/Onboarding.ps1
@@ -1,3 +1,82 @@
+<#
+
+.SYNOPSIS
+    A one sentence explanation of the script.
+
+.DESCRIPTION
+    Information Collected
+    |-> First Name
+        Last Name
+        Location and Department (Dropdown)
+        Fax Access? (Multiple Choice)
+
+    Cycle through existing users until one is open for the username
+    Create User with Name and EV1 password, set to change next login
+    Assign email address based on name
+    Assign Address, Fax, and Phone based on Location/Department
+    Assign description based on Title/Location
+    Fill in Organization, Title, Office, Web Page, Department
+    Assign Groups and Distribution based on Model account based on Location/Department
+    Move to OU based on Description
+    Sync with Azure
+    Assign Licsense
+    Give Fax Memberships
+    Add zoom basic
+
+
+.EXAMPLE
+    Example
+
+.NOTES
+    General notes
+#>
+
+
+#<<<<<<<<<<<<<<<<<<<<      Functions and Parameters     >>>>>>>>>>>>>>>>>>>#
+
+<#
+param (
+    [string]$server = "http://defaultserver", #Parameter of -server with a default value
+    [Parameter(Mandatory = $true)][string]$default, #Parameter that the script will not run without getting. User will be prompted if not given
+    [switch]$force = $false #switch that can be checked for later
+)
+#>
+
+function Test-Administrator {
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+<#function name {
+    param ([type]$parameter1, [type]$parameter2) #Function Parameters
+    #Function Body
+}#>
+
+#<<<<<<<<<<<<<<<<<<<<      Admin Check      >>>>>>>>>>>>>>>>>>>#
+
+if (-NOT (Test-Administrator)) {
+    Write-Host 'Error : Permission Denied : Elevation is required' -ForegroundColor Red
+    Write-Host 'Please run as Administrator' -ForegroundColor Red
+    exit
+}
+
+#<<<<<<<<<<<<<<<<<<<<      Variables and Interaction      >>>>>>>>>>>>>>>>>>>#
+
+$UserCredential = Get-Credential
+$InputFromUser = Read-Host -Prompt 'Ask for something'
+
+#<<<<<<<<<<<<<<<<<<<<      Script Body      >>>>>>>>>>>>>>>>>>>#
+Set-ExecutionPolicy RemoteSigned
+Write-Output $UserCredential
+Write-Output $InputFromUser
+Write-Output $server
+Write-Output $default
+
+# switch example using $force
+if ($force) {
+    Write-Host 'I did the thing'
+}
+
 
 #Create Window
 Add-Type -assembly System.Windows.Forms
