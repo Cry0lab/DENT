e224c72a153c4c7c941dcb8fffc4342307db909e
Daniel Molina <dan-molina@outlook.com>
2018-11-21 10:22:18 -0600

    DHCP renew

--- Repair-PC/Testing.ps1
+++ Repair-PC/Testing.ps1
@@ -1,30 +1,5 @@
-<#
-
-.SYNOPSIS
-    A module to automatically repair common issues on a Domain Joined Windows 10 machine.
-
-.DESCRIPTION
-    This module will accomplish the following functions in the following order unless specified not to execute:
-        Flush Dns
-        DHCP Renew
-        Windows Credentials Flush
-        Enable Netlogon Service and set to automation
-        Gpupdate /force
-        Reboot
-    The executable will exist on the C: Drive with a shortcut on the Desktop
-.EXAMPLE
-    Example
-
-.NOTES
-    General notes
-#>
-
-
-#<<<<<<<<<<<<<<<<<<<<      Functions and Parameters     >>>>>>>>>>>>>>>>>>>#
-
-
 param (
-    #    [Parameter(Mandatory = $true)][string]$default, #Parameter that the script will not run without getting. User will be prompted if not given
+    #[Parameter(Mandatory = $true)][string]$default, #Parameter that the script will not run without getting. User will be prompted if not given
     [switch]$NoFlushDns = $false,
     [switch]$NoIPRenew = $false,
     [switch]$KeepCreds = $false,
@@ -33,34 +8,9 @@ param (
     [switch]$NoReboot = $false
 )
 
-function check-module {
-    param {
-        [Parameter(Mandatory = $true)][string]$moduleName
-    }
-    Write-Host 'Veryifing that $moduleName is installed....' -ForegroundColor Yellow
-
-    if (Get-Module -ListAvailable -Name $moduleName) {
-        Write-Host 'Success: $moduleName is installed' -ForegroundColor Green
-    }
-    else {
-        Write-Host "Failure: $moduleName does not exist" -ForegroundColor Red
-        Write-Host "Installing $moduleName Module...." -ForegroundColor Yellow
-        Install-Module $moduleName -force
-        Write-Host "Module installed" -ForegroundColor Green
-    }
-}
-
-function flush-dns {
-    if ($NoFlushDns) {
-        Write-Host
-        Write-Host "Skipping DNS Flush" -ForegroundColor Green
-        Write-Host
-    }
-    else {
-        Write-Host "Flushing DNS" -ForegroundColor Yellow
-        Clear-DnsClientCache
-        Write-Host "DNS Flushed" -ForegroundColor Green
-    }
+function Test-Administrator {
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
 
 function renew-dhcp {
@@ -103,38 +53,9 @@ function renew-dhcp {
 
 }
 
-function flush-creds {
-    if (check-module "CredentialManager") {
-        #Flush the Creds
-    }
-    else {
-        #Install the Module
-        #Flush the creds
-    }
-}
-
-function set-Netlogon {
-    Set-Service -Name Netlogon -StartupType Automatic
-    Start-Service -Name Netlogon
-}
-
-
-function Test-Administrator {
-    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
-    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-}
-
-#<<<<<<<<<<<<<<<<<<<<      Admin Check      >>>>>>>>>>>>>>>>>>>#
-
 if (-NOT (Test-Administrator)) {
     Write-Host 'Error : Permission Denied : Elevation is required' -ForegroundColor Red
     Write-Host 'Please run this Script as Administrator' -ForegroundColor Red
     exit
 }
-
-
-#<<<<<<<<<<<<<<<<<<<<      Variables and Interaction      >>>>>>>>>>>>>>>>>>>#
-
-
-
-#<<<<<<<<<<<<<<<<<<<<      Script Body      >>>>>>>>>>>>>>>>>>>#
+renew-dhcp
